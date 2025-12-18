f0f271e3b3e7fa9b344fc9b7987f07fcb55ca715
osetter <vovalistik@bk.ru>
2025-12-18 22:27:40 +0300

    feat(agents): task 2 completed

--- server-objects/src/agents/task_2.ts
+++ server-objects/src/agents/task_2.ts
@@ -1,18 +1,18 @@
+﻿
 /* --- types --- */
 interface IError {
-	code: number;
-	message: string;
+    code: number;
+    message: string;
+}
+
+interface ISectionData {
+    title: string;
+    date: string;
+    description: string;
+    access_group: string;
 }
 
 /* --- utils --- */
-/**
- * Выбирает все записи sql запроса
- * @param {string} query - sql-выражение
- */
-function selectAll<T>(query: string) {
-	return ArraySelectAll<T>(tools.xquery(`sql: ${query}`));
-}
-
 /**
  * Создает поток ошибки с объектом error
  * @param {object} source - источник ошибки
@@ -23,18 +23,42 @@ function HttpError(source: string, error: IError) {
 }
 
 /* --- logic --- */
-function getColls() {
-	try {
-		selectAll("SELECT * FROM collaborators");
-	} catch (err) {
-		HttpError("getColls", err as IError);
+function createPortalCard(data: ISectionData) {
+    try {
+		const docNew = tools.new_doc_by_name<DocumentDocument>('document');
+		docNew.BindToDb();
+		const te = docNew.TopElem;
+
+		te.name.Value = data.title;
+        te.comment.Value = data.description;
+		te.create_date.Value = OptDate(data.date);
+		te.parent_document_id.Value = OptInt(GLOBAL.PARENT_DOCUMENT_ID);
+        te.access.access_groups.ObtainChildByKey(OptInt(data.access_group));
+
+		docNew.Save();
+
+		log("Успешно создана карточка: " + data.title);
+    } catch (err) {
+        log("Не удалось создать карточку '" + data.title + "': " + err, "error");
+    }
+}
+
+function processImport() {
+	// let filePath = Screen.AskFileOpen()
+	let file = LoadUrlData(GLOBAL.FILE_PATH);
+	file = ParseJson(file)
+	
+	let item: ISectionData
+	// @ts-ignore жалуется на in .. вместо of ..
+	for (item in file) {
+		createPortalCard(item)
 	}
 }
 
 /* --- start point --- */
 function main() {
 	try {
-		getColls();
+		processImport();
 	} catch (err) {
 		log("Выполнение прервано из-за ошибки: main -> " + err, "error");
 	}
@@ -42,7 +66,9 @@ function main() {
 
 /* --- system --- */
 const GLOBAL = {
-	IS_DEBUG: tools_web.is_true(Param.IS_DEBUG)
+	IS_DEBUG: tools_web.is_true(Param.IS_DEBUG),
+	FILE_PATH: String(Param.FILE_PATH),
+	PARENT_DOCUMENT_ID: Param.PARENT_DOCUMENT_ID
 };
 
 const logConfig = {
@@ -74,10 +100,10 @@ function log(message: string, type?: string) {
 	}
 }
 
-log("--- Начало. Агент {название агента} ---");
+log("--- Начало. Агент импорта карточек разделов ---");
 
 main();
 
-log("--- Конец. Агент {название агента} ---");
+log("--- Конец. Агент импорта карточек разделов ---");
 
 export {};
