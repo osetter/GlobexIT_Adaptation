7fcd4ca78ddc3ff486b041435958d5fb0490281a
osetter <vovalistik@bk.ru>
2025-12-29 16:26:58 +0300

    Обновление задания: исправлена логика получения статуса адаптаций

--- server-objects/src/remote-collections/task.ts
+++ server-objects/src/remote-collections/task.ts
@@ -1,4 +1,5 @@
 declare let RESULT: ResultObject[];
+declare let MESSAGE: string;
 
 interface ResultObject {
   id: number;
@@ -13,11 +14,19 @@ interface ResultObject {
 function showAdaptations() {
   const query = `
     SELECT cr.* 
-    FROM dbo.career_reserves cr 
-    WHERE cr.status = 'passed' 
-        AND cr.position_type = 'adaptation'
+    FROM dbo.career_reserves cr
+    INNER JOIN dbo.career_reserve cr_xml ON cr.id = cr_xml.id
+    WHERE cr.position_type = 'adaptation'
+        AND (xpath('count(/career_reserve/tasks/task[type="stage" and status!="passed"])', cr_xml.data))[1]::text::int = 0
+        AND (xpath('count(/career_reserve/tasks/task[type="stage"])', cr_xml.data))[1]::text::int > 0
   `;
   return ArraySelectAll<ResultObject>(tools.xquery(`sql: ${query}`));
 }
 
-RESULT = showAdaptations()
+try {
+  RESULT = showAdaptations();
+  MESSAGE = "Адаптации успешно получены";
+} catch (err) {
+  RESULT = [];
+  MESSAGE = "Ошибка при получении адаптаций";
+}
