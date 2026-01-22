6a8981e166e31eab7520bb13fa6a8ae1b61df3b8
osetter <vovalistik@bk.ru>
2026-01-22 05:46:13 +0300

    feat(api): task 2 updated

--- server-objects/src/api/task_2/task_2.js
+++ server-objects/src/api/task_2/task_2.js
@@ -1,71 +1,70 @@
 /**
- * @namespace Websoft.WT.Person
+ * @namespace Websoft.WT.Person_API
+ * @description API для получения данных о сотрудниках организации
  */
+
 /**
- * @typedef {Object} Employee
- * @property {string} id
- * @property {string} fullname
- * @property {string} login
- * @property {string} email
+ * @typedef {Object} EmployeeInfo
+ * @property {string} id - Идентификатор сотрудника
+ * @property {string} code - Табельный номер
+ * @property {string} fullname - ФИО сотрудника
+ * @property {string} login - Учетная запись
+ * @property {string} email - Электронная почта
+ * @property {string} position_name - Должность
  */
+
 /**
- * @typedef {Object} EmpInfo
- * @property {Employee[]} employees
+ * @typedef {Object} EmployeesResponse
+ * @property {number} error - Статус ошибки (0 - успех)
+ * @property {string} errorText - Описание ошибки
+ * @property {boolean} result - Флаг успешного выполнения
+ * @property {EmployeeInfo[]} array - Список сотрудников
  */
+
 /**
- * @typedef {Object} GetListResult
- * @property {EmpInfo} empinfo
+ * Преобразует значение в строку, возвращает пустую строку для null/undefined
+ * @param {*} val - Исходное значение
+ * @returns {string} Безопасное строковое значение
  */
-/**
- * Получить список сотрудников построчно.
- * @function Get_list
- * @memberof Websoft.WT.Person
- * @returns {GetListResult} Объект с массивом сотрудников
- */
-function _get_value(value) {
-	try {
-		return value == undefined ? "" : String(RValue(value));
-	} catch (e) {
-		return value == undefined ? "" : String(value);
-	}
+function toSafeString(val) {
+	return val != null ? String(val) : "";
 }
 
-function _get_collaborators() {
-	return ArraySelectAll(
-		tools.xquery(
-			"for $elem in collaborators return $elem/Fields('id','fullname','login','email')"
-		)
-	);
-}
-
-function Get_list() {
-	var collabList = _get_collaborators();
-	var employees = [];
-	var collabItem;
-	var collabId;
-	var collabFullname;
-	var collabLogin;
-	var collabEmail;
-
-	for (var i = 0; i < ArrayCount(collabList); i++) {
-		collabItem = collabList[i];
-		collabId = _get_value(collabItem.id);
-		collabFullname = _get_value(collabItem.fullname);
-		collabLogin = _get_value(collabItem.login);
-		collabEmail = _get_value(collabItem.email);
-
-		employees.push({
-			id: collabId,
-			fullname: collabFullname,
-			login: collabLogin,
-			email: collabEmail
-		});
-	}
-
-	return {
-		empinfo: {
-			employees: employees
-		}
+/**
+ * Возвращает список всех сотрудников из базы
+ * @function Get_All_Employees
+ * @memberof Websoft.WT.Person_API
+ * @returns {EmployeesResponse} Объект с результатом и массивом сотрудников
+ */
+function Get_All_Employees() {
+	var response = {
+		error: 0,
+		errorText: "",
+		result: true,
+		array: []
 	};
-}
 
+	try {
+		var allCollaborators = tools.xquery("for $elem in collaborators return $elem");
+		var iCollaborator;
+		var objItem;
+
+		for (iCollaborator in allCollaborators) {
+			objItem = {
+				id: toSafeString(iCollaborator.id.Value),
+				code: toSafeString(iCollaborator.code.Value),
+				fullname: toSafeString(iCollaborator.fullname.Value),
+				login: toSafeString(iCollaborator.login.Value),
+				email: toSafeString(iCollaborator.email.Value),
+				position_name: toSafeString(iCollaborator.position_name.Value)
+			};
+			response.array.push(objItem);
+		}
+	} catch (ex) {
+		response.error = 1;
+		response.errorText = "Не удалось получить список сотрудников: " + String(ex);
+		response.result = false;
+	}
+
+	return response;
+}
